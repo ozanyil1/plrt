406ce20e502d150d9b32612fc64b02861babe456
ozanyil1 <60675637+ozanyil1@users.noreply.github.com>
2021-03-10 10:37:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,28 +5,33 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 }
 
 const dealinput = document.getElementById('dealinput');
+const agentinput = document.getElementById('agentinput');
 
 const hesapla = document.getElementById('hesapla')
-hesapla.onclick = function(){
+hesapla.onclick = async function(){
     
-    const reader = new FileReader();
-    reader.onload = function(){ 
+    let deals = []
+    let table = {}
+
+    const reader = await new FileReader();
+    
+    reader.onload = await function(){ 
         //var string = new TextDecoder().decode(reader.result);
-        //console.log(string)
         
         
-        let table = {}
-        const lines = reader.result.split("\n");
-        lines.shift();
-        lines.shift();
-        lines.pop();
-        lines.pop();
+        
+        
+        deals = reader.result.split("\n");
+        deals.shift();
+        deals.shift();
+        deals.pop();
+        deals.pop();
 
         
         
 
-        for(i=0;i<lines.length;i++) {
-            let lineelement = lines[0];
+        for(i=0;i<deals.length;i++) {
+            let lineelement = deals[0];
             lineelement = lineelement.split("	");
             lineelement.pop();
             lineelement.pop();
@@ -43,46 +48,22 @@ hesapla.onclick = function(){
 
             
             
-            if (table[lineelement[2]] === undefined) {table[lineelement[2]] = {Login:lineelement[2],Markupkar:0,Swapkar:0,Komisyonkar:0}}
+            if (table[lineelement[2]] === undefined) {table[lineelement[2]] = {Login:lineelement[2],Markupkar:0,Swapkar:0,Komisyonkar:0,AgentKom:0}}
             table[lineelement[2]]["Swapkar"] = Math.round((table[lineelement[2]]["Swapkar"] + (lineelement[9] * -1)) * 100) / 100
             table[lineelement[2]]["Komisyonkar"] = Math.round((table[lineelement[2]]["Komisyonkar"] + (lineelement[8] * -1)) * 100) / 100
-            lines.shift();
-            lines.push(lineelement);
+            deals.shift();
+            deals.push(lineelement);
         }
 
 
-        console.log("lines bitti")
+        
         
 
-        for (i=0;i<Object.keys(table).length;i++){
-            let tablerow = document.createElement("tr");
-            
-            let th1 = document.createElement("th")
-            th1.innerHTML = table[Object.keys(table)[i]]["Login"];
-            tablerow.appendChild(th1)
-
-            let th2 = document.createElement("th")
-            th2.innerHTML = table[Object.keys(table)[i]]["Swapkar"] + "$";
-            tablerow.appendChild(th2)
-
-            let th3 = document.createElement("th")
-            th3.innerHTML = table[Object.keys(table)[i]]["Komisyonkar"] + "$";
-            tablerow.appendChild(th3)
-
-            let th4 = document.createElement("th")
-            th4.innerHTML = table[Object.keys(table)[i]]["MarkupKar"] + "$";
-            tablerow.appendChild(th4)
-
-
-            
-            document.getElementById("ozettable").appendChild(tablerow)
-        }
-
-        console.log("table bitti")
+        
         
     }
 
-    reader.addEventListener('progress',(eve) => {
+    await reader.addEventListener('progress',(eve) => {
         if(eve.loaded && eve.total) {
             const percent = (eve.loaded / eve.total)*100;
             console.log('Progress:' + Math.round(percent))
@@ -90,6 +71,77 @@ hesapla.onclick = function(){
     })
 
     let blob = dealinput.files[0]
-    reader.readAsText(blob);
+    await reader.readAsText(blob);
+
+    const reader2 = new FileReader();
+    reader2.onload = await function() {
+        let agents = reader2.result.split("\n");
+        
+        agents.shift();
+        agents.shift();
+        agents.pop();
+        agents.pop();
+
+
+        for(i=0;i<agents.length;i++) {
+            let lineelement = agents[0];
+            
+            lineelement = lineelement.split("	");
+            lineelement.pop();
+            lineelement.shift();
+            lineelement.shift();
+            lineelement.shift();
+            lineelement.shift();
+            lineelement.shift();
+            lineelement.push(lineelement[0].split("#")[1]);
+            lineelement.push(parseFloat(lineelement[0].split("#")[0].split("'")[1]));
+            lineelement.shift();
+            
+            agents.shift();
+            agents.push(lineelement);
+
+            if (table[lineelement[2]] === undefined) {table[lineelement[2]] = {Login:lineelement[2],Markupkar:0,Swapkar:0,Komisyonkar:0,AgentKomisyon:0}}
+            table[lineelement[2]]["AgentKom"] = Math.round((table[lineelement[2]]["AgentKom"] + parseFloat(lineelement[0])) * 100) / 100
+
+        }
+        
+        for (i=0;i<Object.keys(table).length;i++){
+            let tablerow = document.createElement("tr");
+            
+            let td1 = document.createElement("td")
+            td1.innerHTML = table[Object.keys(table)[i]]["Login"];
+            tablerow.appendChild(td1)
+    
+            let td2 = document.createElement("td")
+            td2.innerHTML = table[Object.keys(table)[i]]["Swapkar"] + "$";
+            tablerow.appendChild(td2)
+    
+            let td3 = document.createElement("td")
+            td3.innerHTML = table[Object.keys(table)[i]]["Komisyonkar"] + "$";
+            tablerow.appendChild(td3)
+    
+            let td4 = document.createElement("td")
+            td4.innerHTML = " ";
+            tablerow.appendChild(td4)
+    
+            let td5 = document.createElement("td")
+            td5.innerHTML = table[Object.keys(table)[i]]["AgentKom"] + "$";
+            tablerow.appendChild(td5)
+            
+            document.getElementById("ozettable").appendChild(tablerow)
+        }
+
+    }
+    let blob2 = agentinput.files[0]
+    await reader2.readAsText(blob2);
+
+    
+
+    console.log(table)
+
+    
+
+
+
         
 }
